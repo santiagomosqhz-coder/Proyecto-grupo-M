3e081f4756b1d65fcddb5da4c7a10ddc0c5b0248
yeison moreno <yeisonmorenoberrio2@gmail.com>
2026-09-23 08:43:39 -0500

    creacion de metodo constructor y metodos

--- jugador.cpp
+++ jugador.cpp
@@ -3,4 +3,25 @@ private:
     int idJugador;
     vector<Carta> mano;
     vector<Carta> cartasGanadas;
-}
+
+    public:
+    Jugador(int idJugador = 0) : idJugador(idJugador) {
+        
+    }
+
+    int getId() const {
+        return idJugador; 
+        
+    }
+
+    void recibirCarta(Carta carta) {
+        mano.push_back(carta);
+    }
+
+    Carta jugarCarta(int posicion) {
+        Carta jugada = mano[posicion];
+        mano.erase(mano.begin() + posicion);
+        return jugada;
+    }    
+
+};
