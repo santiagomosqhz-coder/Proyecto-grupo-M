a18bbd7033943b058f3d00bdc2775198f01ff998
yeison moreno <yeisonmorenoberrio2@gmail.com>
2026-09-25 18:12:26 -0500

    modificacion de la clasae

--- include/jugador.h
+++ include/jugador.h
@@ -3,24 +3,3 @@
 
 #include <vector>
 #include "Carta.h"
-class jugador {
-
-    int idJugador;
-    std::vector<Carta> mano;
-    std::vector<Carta> cartasGanadas;
-
-public:
-    Jugador(int idJugador = 0);
-    int getId() const;
-
-    void recibirCarta(Carta carta);
-    Carta jugarCarta(int posicion);
-    void agregarCartaGanada(Carta carta);
-
-    int getNumeroCartas() const;
-    int getPuntos() const;
-
-    void mostrarMano() const;
-    bool tieneCartasDeColor(Color color) const;
-}
-#endif
