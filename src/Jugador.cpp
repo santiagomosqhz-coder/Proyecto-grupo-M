28a35b2e2b5b2b9b48d54fec12a3a1809e3df07b
yeison moreno <yeisonmorenoberrio2@gmail.com>
2026-09-25 18:45:22 -0500

    creacion de metodo recibirCartas

--- src/Jugador.cpp
+++ src/Jugador.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 
 Jugador::Jugador(int idJugador) : idJugador(idJugador) {}
+int Jugador::getId() const {
+    return idJugador;
+}
+
+void Jugador::recibirCarta(Carta carta) {
+    mano.push_back(carta);
+}
