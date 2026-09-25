e06cbf5b265d3be2ad9af63ca6a202869d64cb02
yeison moreno <yeisonmorenoberrio2@gmail.com>
2026-09-25 18:47:48 -0500

    creacion de metodo jugarcarta y agregarcartaganada

--- src/Jugador.cpp
+++ src/Jugador.cpp
@@ -10,3 +10,12 @@ int Jugador::getId() const {
 void Jugador::recibirCarta(Carta carta) {
     mano.push_back(carta);
 }
+Carta Jugador::jugarCarta(int posicion) {
+    Carta jugada = mano[posicion];
+    mano.erase(mano.begin() + posicion);
+    return jugada;
+}
+
+void Jugador::agregarCartaGanada(Carta carta) {
+    cartasGanadas.push_back(carta);
+}
